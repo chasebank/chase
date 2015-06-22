3d5dd2c450ebe7b32d4766206d973d93fa696aab
Chase Whiteside <chasesbank@gmail.com>
2015-06-22 19:20:07 -0400

    abstracting JS into functions.js begin work on navigation

---
+++ public/js/functions.js
@@ -0,0 +1,54 @@
+(function(document) {
+    var toggle = document.querySelector('.sidebar-toggle');
+    var sidebar = document.querySelector('#sidebar');
+    var checkbox = document.querySelector('#sidebar-checkbox');
+
+    document.addEventListener('click', function(e) {
+      var target = e.target;
+
+      if(!checkbox.checked ||
+         sidebar.contains(target) ||
+         (target === checkbox || target === toggle)) return;
+
+      checkbox.checked = false;
+    }, false);
+})(document);
+
+$(document).ready(
+
+  function() { 
+
+    $("html").niceScroll();
+
+  }
+
+);
+
+$("html").getNiceScroll().hide();
+
+$(function() {
+  /* ADDED: make targets focusable */
+  $('target[id]').attr('tabindex', '-1');
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    var $linkElem = $(this);
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          /* ADDED: focus the target */
+          target.focus();
+          /* end ADDED */
+          /* ADDED: update the URL */
+          window.location.hash = $linkElem.attr('href').substring(1);
+         // window.location.hash = $(this).attr('href').substring(1, $(this).attr('href').length);
+          /* end ADDED */
+        });
+        return false;
+      }
+    }
+  });
+});
