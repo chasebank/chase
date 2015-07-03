cda6a7122f27f041e61f98806e20a010f8413ac0
Chasew <gMobious78B>
2015-07-03 03:34:07 -0400

    contact form

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,37 +1,17 @@
-/*(function(document) {
-    var toggle = document.querySelector('.sidebar-toggle');
-    var sidebar = document.querySelector('#sidebar');
-    var checkbox = document.querySelector('#sidebar-checkbox');
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+/*jslint white: true */
 
-    document.addEventListener('click', function(e) {
-      var target = e.target;
-
-      if(!checkbox.checked ||
-         sidebar.contains(target) ||
-         (target === checkbox || target === toggle)) return;
-
-      checkbox.checked = false;
-    }, false);
-})(document);*/
-
-$(document).ready(
-
-  function() { 
-
-    $("html").niceScroll();
-
-  }
-
-);
 
 $(function() {
+  "use strict";
   /* ADDED: make targets focusable */
   $('target[id]').attr('tabindex', '-1');
 
   $('a[href*=#]:not([href=#])').click(function() {
-    var $linkElem = $(this);
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
+    var $linkElem = $(this),
+        target = $(this.hash);
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
@@ -49,4 +29,68 @@ $(function() {
       }
     }
   });
+});
+
+
+
+
+// Float Label (slightly modified) based on version by Aaron Barker www.codepen.io/aaronbarker/
+$( function(){
+  "use strict";
+  var activeClass = "active",
+      showClass = "show";
+  
+  $("input, textarea").bind("checkval",function(){
+    var label = $(this).prevAll("label");
+    if(this.value !== ""){
+      label.addClass(showClass);
+    } else {
+      label.removeClass(showClass);
+    }
+  })
+  .on("keyup",function(){
+    $(this).trigger("checkval");
+  })
+  .on("focus",function(){
+    $(this).prevAll("label").addClass("active show");
+  })
+  .on("blur",function(){
+		$(this).prevAll("label").removeClass(activeClass);
+  })
+  .trigger("checkval");
+});
+
+
+// Helper Label by Chase Whiteside www.codepen.io/chasebank/
+$( function(){
+  "use strict";
+  var hideClass = "hide";
+  
+  $('.helper').hide().addClass('hide');
+  
+  $("input, textarea").bind("checkval",function(){
+    var label = $(this).prevAll("label"),
+        labelMargin = label.width(),
+        helper = $(this).nextAll(".helper");
+    if(this.value !== ""){
+      helper.css("margin-left",labelMargin+10);
+    } else {
+      helper.removeAttr('style');
+    }
+  })
+  .on("keyup",function(){
+    $(this).trigger("checkval");
+  })
+  .on("focus",function(){
+    $(this).trigger("checkval").nextAll(".helper").removeClass(hideClass);
+  })
+  .on("blur",function(){
+		$(this).nextAll(".helper").removeAttr('style').addClass(hideClass);
+  }).trigger("checkval");
+});
+
+
+$("#validate").click(function(){
+    "use strict";
+    $('#name').closest('li').addClass('error');
 });
