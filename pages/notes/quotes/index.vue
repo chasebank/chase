e783f13649127565765f91137f35469ffab0210d
Chase Whiteside <chasesbank@gmail.com>
2019-02-13 23:30:35 -0500

    building out quotes

---
+++ pages/notes/quotes/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="container">
+    <div class="content">
+      <h1><span>Notes</span><span>& Quotes</span></h1>
+
+      <quotes/>
+    </div>
+  </main>
+</template>
+
+<script>
+import quotes from '@/components/quotes-component.vue'
+import pageMixin from '@/mixins/page-mixin.vue'
+
+export default {
+  components: {
+    quotes
+  },
+
+  data() {
+    return {
+      title: 'Notes & Quotes',
+    }
+  },
+
+  mixins: [pageMixin],
+
+  meta: {
+    depth: 2
+  },
+}
+</script>
