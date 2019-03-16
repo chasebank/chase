4e3f04e6541f3e2fe042502f36c087e40089e065
Chase Whiteside <chasesbank@gmail.com>
2019-03-15 23:57:54 -0400

    start of book component

---
+++ components/book-component.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="book-content">
+      <span class="book-title">{{ book.title }}</span>
+      <span class="book-authors">{{ book.authors }}</span>
+      <span class="book-content" v-html="book.__content"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['book'],
+
+  data: () => ({
+
+  }),
+
+  mounted() {
+    this.documentElement.style.setProperty('--bookHue', this.book.hue);
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
