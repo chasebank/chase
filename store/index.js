2160db9883ab75e83db0c215ebab93ed1e9d3f86
Chase Whiteside <chasesbank@gmail.com>
2019-05-16 21:23:41 -0400

    change vuex store to modules in prep for deprecation of classic mode in Nuxt 3

--- store/index.js
+++ store/index.js
@@ -1,58 +1,50 @@
-import Vuex from 'vuex';
+export const state = () => ({
+  contentScrolled: false,
+  currentRouteTitle: '',
+  routeHistory: '',
+  routeDepth: '1',
+  routeTransitionDirection: 'transition--route-slide-right-'
+})
 
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      contentScrolled: false,
-      currentRouteTitle: '',
-      routeHistory: '',
-      routeDepth: '1',
-      routeTransitionDirection: 'transition--route-slide-right-'
-    },
-    
-    mutations: {
-      contentScrolled(state) {
-        state.contentScrolled = true
-        console.log('set to true ')
-      },
+export const mutations = {
+  contentScrolled(state) {
+    state.contentScrolled = true
+    console.log('set to true ')
+  },
 
-      contentNotScrolled(state) {
-        state.contentScrolled = false
-        console.log('set to false ')
-      },
+  contentNotScrolled(state) {
+    state.contentScrolled = false
+    console.log('set to false ')
+  },
 
-      handleHistory(state, payload) {
-        if (typeof payload !== "undefined") {
-  
-          if (state.routeHistory[state.routeHistory.length - 1] == payload) {
-            state.routeHistory.pop()
-            // console.log('popped')
-          }
-          
-          else if (state.currentRouteTitle != '') {
-            state.routeHistory.push(state.currentRouteTitle)
-          }
-  
-          state.currentRouteTitle = payload
-  
-        } else {
-          state.currentRouteTitle = ''
+  handleHistory(state, payload) {
+    if (typeof payload !== "undefined") {
 
-          state.routeHistory = ["Home"]
-        }
-      },
-
-      removeHistory(state) {
+      if (state.routeHistory[state.routeHistory.length - 1] == payload) {
         state.routeHistory.pop()
-      },
-
-      setRouteTransitionDirection(state, newRouteDepth) {
-        newRouteDepth < state.routeDepth ? state.routeTransitionDirection = 'transition--route-slide-right-' : state.routeTransitionDirection = 'transition--route-slide-left-'
-        
-        state.routeDepth = newRouteDepth
+        // console.log('popped')
       }
-    }
-  })
-}
 
-export default createStore
+      else if (state.currentRouteTitle != '') {
+        state.routeHistory.push(state.currentRouteTitle)
+      }
+
+      state.currentRouteTitle = payload
+
+    } else {
+      state.currentRouteTitle = ''
+
+      state.routeHistory = ["Home"]
+    }
+  },
+
+  removeHistory(state) {
+    state.routeHistory.pop()
+  },
+
+  setRouteTransitionDirection(state, newRouteDepth) {
+    newRouteDepth < state.routeDepth ? state.routeTransitionDirection = 'transition--route-slide-right-' : state.routeTransitionDirection = 'transition--route-slide-left-'
+
+    state.routeDepth = newRouteDepth
+  }
+}
