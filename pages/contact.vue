ae8ef63dcee6fc17e31a0d576f1faf7f57d66db2
Chase Whiteside <chasesbank@gmail.com>
2020-01-10 21:38:20 -0500

    contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -5,7 +5,26 @@
 
       <p>Thanks so much for visiting my site! If you'd like to get in touch with me, please use the form below.</p>
       
-      <!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet repudiandae tempora quod architecto autem. Modi necessitatibus non vitae dolor consequatur quaerat quae, iure quidem voluptate officia optio nobis accusantium hic.</p> -->
+      <div id="wufoo-rju92v0267kmo">
+        Fill out my <a href="https://chasew.wufoo.com/forms/rju92v0267kmo">online form</a>.
+      </div>
+      
+      <script type="text/javascript">var rju92v0267kmo;(function(d, t) {
+      var s = d.createElement(t), options = {
+      'userName':'chasew',
+      'formHash':'rju92v0267kmo',
+      'autoResize':true,
+      'height':'500',
+      'async':true,
+      'host':'wufoo.com',
+      'header':'show',
+      'ssl':true};
+      s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'secure.wufoo.com/scripts/embed/form.js';
+      s.onload = s.onreadystatechange = function() {
+      var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+      try { rju92v0267kmo = new WufooForm();rju92v0267kmo.initialize(options);rju92v0267kmo.display(); } catch (e) {}};
+      var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+      })(document, 'script');</script>
     </div>
   </main>
 </template>
